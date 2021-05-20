1af59dfd503b102fb0f00b02823e01269c3d056f
rogoznik <nikrogoz@gmail.com>
2021-05-20 18:07:37 +0500

    start homework 7

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('products/', include(urls, namespace='products'),),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin_staff/', include('adminapp.urls', namespace='admin_staff')),
 ]
 
 if settings.DEBUG:
